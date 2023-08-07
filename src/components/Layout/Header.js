ac794f551c95da1aea852230330d57a7a474ea06
ChappaAbhiram <abhiramchappa@gmail.com>
2023-08-07 17:11:14 +0530

    Added restaurant header with cart.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+ import React from 'react';
+ import mealsImage from '../../assets/intro-1670029550.webp';
+ import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+ const Header = (props)=>{
+  return <React.Fragment>
+    <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+    </header >
+    <div className={classes['main-image']}>
+        <img src={mealsImage} alt="Table full of food"/>
+    </div>
+  </React.Fragment>
+ }
+ export default Header;
