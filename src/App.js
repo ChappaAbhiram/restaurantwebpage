a0bf1e55aee455b28775d62d7c1794afb03ebe1a
ChappaAbhiram <abhiramchappa@gmail.com>
2023-08-08 20:14:05 +0530

    Added cart functionality with dummy data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Header from './components/Layout/Header';
-import {Fragment} from 'react';
+import {Fragment, useState} from 'react';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart'
 function App() {
-  return (
+  const [cartisShown,setCartisShown] = useState(false);
+  const showCartHandler = () =>{
+    setCartisShown(true);
+  };
+  const HideCardHandler = () =>{
+    setCartisShown(false);
+  };
+  return ( 
     <Fragment>
-      <Cart />
-      <Header />
+      {cartisShown && <Cart onClose={HideCardHandler}/>}
+      <Header onShowCart={showCartHandler} onHideCart={HideCardHandler}/>
       <main>
       <Meals />
       </main>
